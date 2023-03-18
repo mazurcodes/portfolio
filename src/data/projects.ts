53cce23da57a4122452a71fd82940cb4f32c9076
Bartlomiej <trafficap@gmail.com>
2023-03-18 14:46:35 +0100

    data for projects with types

---
+++ src/data/projects.ts
@@ -0,0 +1,15 @@
+import type { ProjectsDataType } from "../types";
+
+export const projectsData: ProjectsDataType = {
+  gifter: {
+    id: 1,
+    title: "Gifter",
+    description: "",
+    overview: "",
+    technologies: ["react", "nextjs"],
+    userFlow: ["main", "search"],
+    colors: ["dddddd", "#d2d2d2"],
+    typography: "Arial",
+    screenPaths: ["@assets/gifter-01.png"],
+  },
+};
--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,16 @@ export type ProjectType = {
   id: number;
   title: string;
   description: string;
+  overview?: string;
+  technologies?: string[];
+  userFlow?: string[];
+  colors?: string[];
+  typography?: string;
+  screenPaths?: string[];
 };
 
 export type ProjectListType = ProjectType[];
+
+export type ProjectsDataType = {
+  [key: string]: ProjectType;
+};
